25b109e9c7a98112d0d69ef8fbb1e9dd1d6df04e
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-14 17:19:51 +0000

    AppreciaView

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import AppreciaView from '@/components/AppreciaView';
 import Navbar from '../components/Nav/Navbar';
 import { auth } from '@/firebase/config';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const user = auth.currentUser;
@@ -13,17 +13,43 @@ export default function Home() {
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
   };
 
+  // This will run one time after the component mounts
+  useEffect(() => {
+    // callback function to call when event triggers
+    const onPageLoad = () => {
+      console.log('page loaded');
+      setIsLoaded(true);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad, false);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, []);
+
+
+  const backgroundStyles = isLoaded ?
+    {} :
+    {
+      width: '100%',
+      position: 'relative',
+      zIndex: 0,
+      background: 'url(/appreciabg.png)',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'none'
+    }
+
   return (
     <div className="w-full">
       <Navbar user={user} />
       <main
         className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat"
-        style={{
-          background: isLoaded ? 'none' : 'url(/appreciabg.png)',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'none',
-        }}
+        style={backgroundStyles}
       >
         <AppreciaView className="h-full w-full" onLoad={handleSplineLoad} />
       </main>

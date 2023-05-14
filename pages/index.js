79811f947a21be2318e3dfa2cd7ab59d5ef43695
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-14 16:32:20 +0000

    remove bg

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,29 @@ import { auth } from '@/firebase/config';
 import { useState } from 'react';
 
 export default function Home() {
-
-  // Get the current user
   const user = auth.currentUser;
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  // const [isLoaded, setIsLoaded] = useState(false);
-  // const backgroundImage = '/appreciabg.png';
+  const handleSplineLoad = () => {
+    setIsLoaded(true);
+    const { gl } = document.getElementsByTagName("canvas")[0];
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  };
 
   return (
     <div className="w-full">
       <Navbar user={user} />
-      {/* onObjectLoad is the event when object is loaded */}
-      <main className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat"
-      // style={{
-      //   background: isLoaded ? 'none' : `url(${backgroundImage})`,
-      //   backgroundPosition: 'center',
-      //   backgroundSize: 'cover',
-      //   backgroundRepeat: 'none'
-      // }}
+      <main
+        className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat"
+        style={{
+          background: isLoaded ? 'none' : 'url(/appreciabg.png)',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'none',
+        }}
       >
-        {/* <AppreciaView className="h-full w-full" onLoad={() => setIsLoaded(true)} /> */}
-        <AppreciaView className="h-full w-full" />
+        <AppreciaView className="h-full w-full" onLoad={handleSplineLoad} />
       </main>
-    </div >
+    </div>
   );
 }

cd682420cd7296c1afe19a5f3787394a6bed5779
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-14 16:27:31 +0000

    remove bg

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,23 @@ export default function Home() {
   // Get the current user
   const user = auth.currentUser;
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  const backgroundImage = '/appreciabg.png';
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const backgroundImage = '/appreciabg.png';
 
   return (
     <div className="w-full">
       <Navbar user={user} />
       {/* onObjectLoad is the event when object is loaded */}
       <main className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat"
-        style={{
-          background: isLoaded ? 'none' : `url(${backgroundImage})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'none'
-        }}
+      // style={{
+      //   background: isLoaded ? 'none' : `url(${backgroundImage})`,
+      //   backgroundPosition: 'center',
+      //   backgroundSize: 'cover',
+      //   backgroundRepeat: 'none'
+      // }}
       >
-        <AppreciaView className="h-full w-full" onLoad={() => setIsLoaded(true)} />
+        {/* <AppreciaView className="h-full w-full" onLoad={() => setIsLoaded(true)} /> */}
+        <AppreciaView className="h-full w-full" />
       </main>
     </div >
   );

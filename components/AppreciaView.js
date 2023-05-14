18d9d07e1e39c36eaabbcd6478339f620a50f53a
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-14 16:02:37 +0000

    AppreciaView

--- components/AppreciaView.js
+++ components/AppreciaView.js
@@ -2,20 +2,7 @@ import Spline from '@splinetool/react-spline';
 import { useState } from 'react';
 
 export default function AppreciaView() {
-    const [isLoaded, setIsLoaded] = useState(false);
-    const backgroundImage = '/appreciabg.png';
-
     return (
-        <main className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat"
-            style={{
-                background: isLoaded ? 'none' : `url(${backgroundImage})`,
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'none'
-            }}
-        >
-            <Spline onLoad={() => setIsLoaded(true)}
-                scene="https://prod.spline.design/UB-SpUKi5AWALdo7/scene.splinecode" />
-        </main>
+        <Spline cene="https://prod.spline.design/UB-SpUKi5AWALdo7/scene.splinecode" />
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,23 @@ export default function Home() {
   // Get the current user
   const user = auth.currentUser;
 
+  const [isLoaded, setIsLoaded] = useState(false);
+  const backgroundImage = '/appreciabg.png';
+
   return (
     <div className="w-full">
       <Navbar user={user} />
       {/* onObjectLoad is the event when object is loaded */}
-      <AppreciaView className="h-full w-full" />
+      <main className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat"
+        style={{
+          background: isLoaded ? 'none' : `url(${backgroundImage})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'none'
+        }}
+      >
+        <AppreciaView className="h-full w-full" onLoad={() => setIsLoaded(true)} />
+      </main>
     </div >
   );
 }

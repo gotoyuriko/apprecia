45d43881b65a29923d801939d9f63da810c643c5
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-18 12:29:42 +0000

    chnaged 3d apprecia view

--- pages/index.js
+++ pages/index.js
@@ -7,43 +7,13 @@ export default function Home() {
   const user = auth.currentUser;
   const [isLoaded, setIsLoaded] = useState(false);
 
-  const handleSplineLoad = () => {
-    setIsLoaded(true);
-    const canvasElement = document.getElementsByTagName('canvas')[0];
-    if (canvasElement) {
-      try {
-        const { gl } = canvasElement;
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-      } catch (error) {
-        console.error('Error binding framebuffer:', error);
-      }
-    }
-  };
-
-  // This will run one time after the component mounts
-  useEffect(() => {
-    // callback function to call when event triggers
-    const onPageLoad = () => {
-      console.log('page loaded');
-      setIsLoaded(true);
-    };
-
-    // Check if the page has already loaded
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-      onPageLoad();
-    } else {
-      document.addEventListener('DOMContentLoaded', onPageLoad);
-      // Remove the event listener when component unmounts
-      return () => document.removeEventListener('DOMContentLoaded', onPageLoad);
-    }
-  }, []);
-
   return (
     <div className="w-full">
       <Navbar user={user} />
       <main className="flex h-96 w-full flex-col items-center justify-center bg-fixed bg-center bg-cover bg-repeat">
         {!isLoaded && (
           <div
+            onLoad={() => setIsLoaded(true)}
             style={{
               width: '100%',
               position: 'relative',
@@ -55,7 +25,7 @@ export default function Home() {
             }}
           />
         )}
-        <AppreciaView className="h-full w-full" onLoad={handleSplineLoad} />
+        <AppreciaView className="h-full w-full" />
       </main>
     </div>
   );

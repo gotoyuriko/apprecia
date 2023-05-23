013c6e5a52568293434f467c4a6df4c3eaf9cb00
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-23 05:59:27 +0000

    Revised Image Uploader Component

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -1,39 +1,65 @@
+import Image from "next/image";
 import { useState } from "react";
 
 const ImageUploader = () => {
+    // Create two state variables using the useState hook:
+    // - selectedImages: An array that will store our uploaded images
+    // - previewImages: An array that will store preview URLs for each image 
     const [selectedImages, setSelectedImages] = useState([]);
     const [previewImages, setPreviewImages] = useState([]);
 
+    // Define a function called handleDrop which takes in an event object (e)
     const handleDrop = (e) => {
         e.preventDefault();
 
+        // Get the list of files that were dropped into the drop zone and convert it into an array
         const files = Array.from(e.dataTransfer.files);
+
+        // Call the handleFiles function with the list of uploaded files
         handleFiles(files);
     };
 
+
+    // Define a function called handleFiles which takes in an array of uploaded files (files)
     const handleFiles = (files) => {
+        // Filter out any non-image files from the list of files using the filter method
         const selected = files.filter((file) => file.type.startsWith("image/"));
+
+        // Create a new array of preview URLs for each image using the map method and URL.createObjectURL
         const selectedPaths = selected.map((file) => URL.createObjectURL(file));
 
+        // Update the state variable selectedImages by appending the new list of selected images
         setSelectedImages((prevImages) => [...prevImages, ...selected]);
+
+        // Update the state variable previewImages by appending the new list of preview URLs
         setPreviewImages((prevImages) => [...prevImages, ...selectedPaths]);
     };
 
+
+    // This function is called when the user selects files using an <input type="file"> element
     const handleFileSelect = (e) => {
+        // We convert the FileList object into a regular array so we can easily work with it
         const files = Array.from(e.target.files);
+        // Call the 'handleFiles' function and pass in the array of selected files as an argument
         handleFiles(files);
     };
 
+    // This function removes a selected image from the list of currently selected images and preview images
     const handleRemove = (index) => {
+        // Create a new array by filtering out the selected image at the specified index
         const updatedSelectedImages = selectedImages.filter((_, i) => i !== index);
+        // Create a new array by filtering out the preview image at the specified index
         const updatedPreviewImages = previewImages.filter((_, i) => i !== index);
 
+        // Update the state variable for selected images with the newly filtered array
         setSelectedImages(updatedSelectedImages);
+        // Update the state variable for preview images with the newly filtered array
         setPreviewImages(updatedPreviewImages);
     };
 
+
     return (
-        <div className="shadow-inner h-1/2 w-full bg-white flex flex-col items-center justify-center">
+        <div className="shadow-inner rounded-lg h-full md:h-1/2 w-full bg-white flex flex-col items-center justify-center">
             <input
                 type="file"
                 id="upload"
@@ -43,7 +69,7 @@ const ImageUploader = () => {
             />
             <label
                 htmlFor="upload"
-                className="bg-blue-500 text-white py-2 px-4 rounded-lg cursor-pointer"
+                className="bg-gray-600 text-white py-2 px-4 rounded-lg cursor-pointer"
             >
                 Select Images
             </label>
@@ -58,7 +84,7 @@ const ImageUploader = () => {
                 {previewImages.map((path, index) => (
                     <div key={index} className="relative h-32 w-32">
                         <div className="bg-white w-full h-full border border-gray-300 overflow-hidden">
-                            <img src={path} alt="Preview" className="w-full h-auto" />
+                            <Image src={path} alt="Preview" fill className="w-full h-auto" />
                         </div>
                         <button
                             className="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 rounded-full"
--- pages/newproject.js
+++ pages/newproject.js
@@ -3,50 +3,74 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useAuth } from '@/firebase/auth/AuthContext';
 import ImageUploader from '@/components/ImageUploader';
+import Footer from '@/components/Footer';
 
 export default function NewProject() {
     const { currentUser } = useAuth();
     const router = useRouter();
 
     useEffect(() => {
-        if (currentUser == null) router.push("/")
-    }, [router, currentUser]);
+        if (!currentUser) {
+            router.push('/');
+        }
+    }, [currentUser, router]);
 
     return (
         <div className="w-full">
             <Navbar user={currentUser} />
-            <h1 className="text-4xl font-bold">Create New Project</h1>
-            <div className="flex flex-row h-screen">
-                <div className='flex-1'>
-                    <ImageUploader />
-                </div>
-                <div className="flex-1 flex flex-col items-center justify-center w-full h-full">
-                    <div>
-                        <h1 className="text-2xl font-bold">Project Title</h1>
-                        <input className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
+            <div className="h-full py-5">
+                <h1 className="text-4xl font-bold text-center lg:text-start py-5 px-10">
+                    Create New Project
+                </h1>
+                <div className="flex flex-col md:flex-row">
+                    <div className="flex-1 px-5 pb-5 lg:p-10">
+                        <ImageUploader />
                     </div>
-                    <div>
-                        <h1 className="text-2xl font-bold">Description</h1>
-                        <textarea className="w-96 h-40 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
-                    </div>
-                    <div>
-                        <h1 className="text-2xl font-bold">Tags</h1>
-                        <input className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
-                    </div>
-
-                    <div>
-                        <h1 className="text-2xl font-bold">Link (Optional)</h1>
-                        <input className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
-                    </div>
-                    <div>
-                        <h1 className="text-2xl font-bold">Skill</h1>
-                        <input className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
-                    </div>
-                    <div>
-                        <button className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline">Create Project</button>
+                    <div className="flex-1 flex flex-col items-center w-full h-full">
+                        <div className="mb-4">
+                            <h1 className="text-xl font-medium">Project Title</h1>
+                            <input
+                                className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                                type="text"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <h1 className="text-xl font-medium">Description</h1>
+                            <textarea
+                                className="w-96 h-40 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                                rows="4"
+                            ></textarea>
+                        </div>
+                        <div className="mb-4">
+                            <h1 className="text-xl font-medium">Tags</h1>
+                            <input
+                                className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                                type="text"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <h1 className="text-xl font-medium">Skill</h1>
+                            <input
+                                className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                                type="text"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <h1 className="text-xl font-medium">Link</h1>
+                            <input
+                                className="w-96 h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                                type="text"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <button className="w-96 h-10 px-3 text-base text-white bg-black border rounded-lg focus:shadow-outline">
+                                Create Project
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Footer />
         </div>
     );
 }

71062a2248eb6b42eb4750bac0daa6158fa8d6b1
Goto Yuriko <gotoyuriko95@gmail.com>
2023-05-29 01:44:31 +0000

    fix package

--- pages/profile.js
+++ pages/profile.js
@@ -1,16 +1,12 @@
 import ArtworkCard from "@/components/ArtworkCard";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Nav/Navbar";
+import { useAuth } from "@/firebase/auth/AuthContext";
 import Image from "next/image";
+import { BiUserCircle } from "react-icons/bi";
 
 export default function Profile() {
-    const currentUser = {
-        username: "John Doe",
-        bio: "Art enthusiast",
-        profileIcon: "/appreciabg.png",
-        savedArt: [], // Array of saved artwork objects
-        likedArt: [] // Array of liked artwork objects
-    };
+    const { currentUser } = useAuth();
 
     return (
         <div className="w-full">
@@ -18,10 +14,19 @@ export default function Profile() {
 
             <div className="container mx-auto py-8">
                 <div className="flex items-center justify-center">
-                    <Image src={currentUser.profileIcon} width={200} height={200} alt="Profile Icon" className="w-16 h-16 rounded-full" />
+                    {currentUser?.photoURL ?
+                        <Image
+                            width={200}
+                            height={200}
+                            src={currentUser.photoURL}
+                            alt="Profile Icon"
+                            className="w-16 h-16 rounded-full"
+                        /> :
+                        <BiUserCircle />
+                    }
                 </div>
 
-                <h1 className="text-2xl font-bold text-center mt-4">{currentUser.username}</h1>
+                <h1 className="text-2xl font-bold text-center mt-4">{currentUser.displayName}</h1>
 
                 <p className="text-center text-gray-600 mt-2">{currentUser.bio}</p>
 

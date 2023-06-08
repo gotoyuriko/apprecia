6351ae304992c7726ee3f55ed0f69bc97e0a39bd
Goto Yuriko <gotoyuriko95@gmail.com>
2023-06-08 07:20:17 +0000

    FAQ update

--- data/data.js
+++ data/data.js
@@ -57,3 +57,39 @@ export const tagOptions = [
     { value: 'Graphic Design', label: 'Graphic Design' },
 ];
 
+
+// FAQ List
+export const faqList = [
+    {
+        "q": "What is Apprecia?",
+        "a": "Apprecia is a web application designed for artists to showcase their project work, including art, design, film, 3D models, and more. It also allows artists to create virtual art galleries where users can explore 360 VR panoramas to interact with and appreciate the artworks."
+    }, {
+        "q": "How can I create an account on Apprecia?",
+        "a": "To create an account on Apprecia, simply visit our website and click on the 'Sign Up' button. You will be prompted to provide some basic information and choose a username and password. Once you complete the registration process, your account will be created."
+    }, {
+        "q": "Can I upload different types of artwork on Apprecia?",
+        "a": "Yes, Apprecia supports various types of artwork, including images, videos, 3D models, and films. You can upload your projects in the appropriate format and showcase them to the Apprecia community."
+    }, {
+        "q": "How do I create a virtual art gallery on Apprecia?",
+        "a": "To create a virtual art gallery on Apprecia, log in to your account and navigate to the 'Create Room' section. There, you can specify the gallery name, description, and upload the artworks you want to include. You can also customize the gallery's appearance and layout to create a unique virtual experience."
+    }, {
+        "q": "Can users interact with the artworks in the virtual art galleries?",
+        "a": "Yes, users can interact with the artworks in the virtual art galleries on Apprecia. They can navigate through the 360 VR panoramas, zoom in and out, view details, read descriptions, and leave comments or feedback on individual artworks."
+    }, {
+        "q": "How can I explore other artists' virtual art galleries on Apprecia?",
+        "a": "To explore other artists' virtual art galleries on Apprecia, you can browse through the gallery directory or use the search functionality to find specific artists or artworks. Once you find a gallery you're interested in, you can enter the VR panorama and start exploring and interacting with the artworks."
+    }, {
+        "q": "Can I sell my artwork on Apprecia?",
+        "a": "Apprecia focuses on showcasing and appreciating artwork rather than facilitating direct sales. However, you can provide information about purchasing options, such as links to external websites or contact details, within the description of your artworks or gallery."
+    }, {
+        "q": "Is Apprecia available on mobile devices?",
+        "a": "Yes, Apprecia is designed to be responsive and accessible on various devices, including desktop computers, tablets, and mobile phones. You can access the web application through a compatible browser on your mobile device and enjoy the virtual art galleries on the go."
+    }, {
+        "q": "Are there any fees for using Apprecia?",
+        "a": "Apprecia offers basic features and functionality for free. However, there may be premium or advanced features in the future that require a paid subscription or one-time fee. Any such fees will be clearly communicated to users, and you can choose whether or not to opt for those additional features."
+    }, {
+        "q": "How can I contact the Apprecia support team?",
+        "a": "If you have any questions, feedback, or need assistance with Apprecia, you can reach out to our support team by visiting the 'Contact Us' page on our website. Fill out the provided form with your query, and our team will respond to you as soon as possible."
+    }
+]
+
--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,45 @@
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Nav/Navbar';
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { faqList } from '@/data/data';
+import { useAuth } from '@/firebase/auth/AuthContext';
+
 export default function Faq() {
+    const { currentUser } = useAuth();
     return (
-        <></>
+        <div className="w-full">
+            <Navbar user={currentUser} />
+            <div className='px-8 lg:px-32 pt-10 pb-12 md:pb-32'>
+                <h1 className='text-4xl md:text-5xl text-center py-3'>
+                    Apprecia FAQs
+                </h1>
+                <Accordion
+                    transition
+                    transitionTimeout={250}
+                    className="w-full px-2 md:px-28 pt-8 md:pt-12 text-left space-y-4 md:space-y-4"
+                >
+                    {faqList.map((item, index) => (
+                        <>
+                            <AccordionItem
+                                key={index}
+                                header={
+                                    <div>
+                                        <h1 className='font-bold md:text-xl text-left'>{item.q}</h1>
+                                    </div>
+                                }
+                            >
+                                <div className='text-sm lg:text-lg text-justify'>
+                                    <p>{item.a}</p>
+                                </div>
+
+                            </AccordionItem>
+                            <hr className="h-px my-8 bg-gray-600 border-0" />
+                        </>
+                    ))}
+                </Accordion>
+            </div>
+            <Footer />
+        </div>
     );
 }

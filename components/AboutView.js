abf7e99d7dc7c99970fff5dfbd8ecbc757b1109c
Goto Yuriko <gotoyuriko95@gmail.com>
2023-06-12 15:09:20 +0000

    Add Animation to About Page

--- components/AboutView.js
+++ components/AboutView.js
@@ -2,8 +2,11 @@ import { aboutFeatures } from '@/data/data';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import AboutCard from './AboutCard';
 import Footer from './Footer';
+import { motion, useScroll } from "framer-motion"
+import { useRef } from 'react';
 
 const AboutView = () => {
+    const scrollRef = useRef(null);
     return (
         <>
             <Parallax pages={3} className="w-full h-full relative top-0 left-0">
@@ -20,13 +23,18 @@ const AboutView = () => {
                 </ParallaxLayer>
                 <ParallaxLayer offset={1} speed={0.3}>
                     <div className='h-screen relative bg-[#d6d6d6] lg:top-[40vh] flex justify-center items-center'>
-                        <div className='w-4/5 md:w-3/5 xl:w-2/5'>
+                        <motion.div
+                            initial={{ opacity: 0, y: 30 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            viewport={{ root: scrollRef }}
+                            transition={{ delay: 0.2 }}
+                            className='w-4/5 md:w-3/5 xl:w-2/5'>
                             <p className='text-justify text-xl'>
                                 Apprecia was born out of a deep appreciation for art and a desire to create a platform that truly celebrates the talent and creativity of artists from around the world.
                                 We understand the challenges artists face in finding suitable spaces to exhibit their work and reaching a wider audience.
                                 That&rsquo;s why we built <b>Apprecia</b> - to provide a digital space that empowers artists to showcase their artwork in an engaging and accessible manner.
                             </p>
-                        </div>
+                        </motion.div>
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={2} speed={0.2}>
@@ -38,7 +46,13 @@ const AboutView = () => {
                                 {
                                     aboutFeatures.map((item, index) => {
                                         return (
-                                            <AboutCard key={index} title={item.featureTitle} des={item.featureDescription} src={item.src} alt={item.alt} />
+                                            <motion.div key={index}
+                                                initial={{ opacity: 0, y: 30 }}
+                                                whileInView={{ opacity: 1, y: 0 }}
+                                                viewport={{ root: scrollRef }}
+                                                transition={{ delay: (0.3 * (index + 1)) }} >
+                                                <AboutCard title={item.featureTitle} des={item.featureDescription} src={item.src} alt={item.alt} />
+                                            </motion.div>
                                         );
                                     })
                                 }

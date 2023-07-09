0d339f8bd61cc2cade0eceb8080eecd7f7e8d5a0
Yuriko Goto <gotoyuriko95@gmail.com>
2023-07-09 17:20:10 +0800

    Added Audio

--- components/VirtualTour/ViewTour/TourTitle.js
+++ components/VirtualTour/ViewTour/TourTitle.js
@@ -1,4 +1,5 @@
 import { BiSolidVolumeFull, BiSolidVolumeMute } from "react-icons/bi";
+
 export default function TourTitle({ tourData, roomNo, muted, setMuted, audioRef, setCurrentTime, currentTime }) {
     const playAudio = () => {
         audioRef.current.currentTime = currentTime;
@@ -25,18 +26,22 @@ export default function TourTitle({ tourData, roomNo, muted, setMuted, audioRef,
             <h1 className="text-2xl font-bold text-white">
                 <span>{tourData?.tour_name}</span> - Room <span>{roomNo}</span>
             </h1>
-            {muted ? (
-                <BiSolidVolumeMute
-                    onClick={handleAudioButtonClick}
-                    className="w-10 h-10 text-white bg-black rounded-full p-2 shadow hover:bg-gray-800"
-                />
-            ) : (
-                <BiSolidVolumeFull
-                    onClick={handleAudioButtonClick}
-                    className="w-10 h-10 text-white bg-black rounded-full p-2 shadow hover:bg-gray-800"
-                />
+            {tourData?.tour_audio !== "" && (
+                muted ? (
+                    <BiSolidVolumeMute
+                        onClick={handleAudioButtonClick}
+                        className="w-10 h-10 text-white bg-black rounded-full p-2 shadow hover:bg-gray-800"
+                    />
+                ) : (
+                    <BiSolidVolumeFull
+                        onClick={handleAudioButtonClick}
+                        className="w-10 h-10 text-white bg-black rounded-full p-2 shadow hover:bg-gray-800"
+                    />
+                )
             )}
-            <audio ref={audioRef} src={tourData?.tour_audio} loop></audio>
+            {tourData?.tour_audio !== "" && (
+                <audio ref={audioRef} src={tourData?.tour_audio} loop></audio>
+            )}
         </div>
     );
 }

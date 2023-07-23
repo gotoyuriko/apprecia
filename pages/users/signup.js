71cdb9650a9b4599aaa97c568937b3b8cd742684
Yuriko Goto <gotoyuriko95@gmail.com>
2023-07-23 22:45:13 +0800

    fix design on sign up

--- pages/users/signup.js
+++ pages/users/signup.js
@@ -20,7 +20,7 @@ export default function SignUp() {
                         priority
                     />
                 </div>
-                <div className="p-8 md:px-0 md:py-12 mx-auto w-full max-w-sm">
+                <div className="p-8 md:px-0 md:py-12 mx-auto w-full max-w-sm h-full">
                     <h1 className="text-2xl font-bold">Sign up to Apprecia</h1>
 
                     <GoogleAuthBtn formStatus='signup' />
@@ -28,9 +28,7 @@ export default function SignUp() {
                     <hr className="mt-8 border-top border-solid border-gray-400 h-4 text-center overflow-visible
                                 after:content-['Or'] after:bg-white after:text-gray-400 after:inline-block after:h-8 after:leading-8 after:relative after:-top-4 after:px-4" />
 
-
                     <AuthForm formStatus='signup' />
-
 
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Already a member?{' '}

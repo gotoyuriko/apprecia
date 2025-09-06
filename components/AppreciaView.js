721510665bcecc9293fb402b1aa2fd1635b4e15e
Goto Yuriko <gotoyuriko95@gmail.com>
2025-09-07 01:09:04 +0800

    Replace Spline with simple gradient placeholder

--- components/AppreciaView.js
+++ components/AppreciaView.js
@@ -1,17 +1,10 @@
-import Spline from "@splinetool/react-spline";
-import { useEffect, useState } from "react";
-
 export default function AppreciaView() {
-    const [isLoaded, setIsLoaded] = useState(false);
-
-    useEffect(() => {
-        setIsLoaded(true);
-    }, []);
-
     return (
-        <div className="h-96" style={{ background: "url(/loading.png) center/cover no-repeat" }}>
-            {isLoaded && (
-                <Spline className="h-full" scene="https://prod.spline.design/UB-SpUKi5AWALdo7/scene.splinecode" />)}
+        <div className="h-96 bg-gradient-to-br from-purple-600 to-blue-500 flex items-center justify-center text-white">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4">Apprecia</h1>
+                <p className="text-xl">Virtual Art Gallery Platform</p>
+            </div>
         </div>
     );
 }

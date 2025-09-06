3404a4bd419f26f426605ddfa29d7c822fa12533
Goto Yuriko <gotoyuriko95@gmail.com>
2025-09-07 00:55:00 +0800

    Isolate issue with minimal context setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,10 @@
-import { 
-  AuthProvider, 
-  UserProvider, 
-  ArtworkProvider, 
-  TourProvider, 
-  CommentProvider, 
-  LikeProvider 
-} from '@/firebase/contexts';
+import { AuthProvider } from '@/firebase/contexts';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <UserProvider>
-        <ArtworkProvider>
-          <TourProvider>
-            <CommentProvider>
-              <LikeProvider>
-                <Component {...pageProps} />
-              </LikeProvider>
-            </CommentProvider>
-          </TourProvider>
-        </ArtworkProvider>
-      </UserProvider>
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }

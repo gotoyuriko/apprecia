789db8b422167ec000633ec24f5a236ed952a112
Goto Yuriko <gotoyuriko95@gmail.com>
2025-09-07 00:57:08 +0800

    Remove all providers to isolate issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import { AuthProvider } from '@/firebase/contexts';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  return <Component {...pageProps} />;
 }

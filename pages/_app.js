7397e671fca78fb4c3f7b36f729ce5a7a4528c65
Goto Yuriko <gotoyuriko95@gmail.com>
2025-09-07 00:33:35 +0800

    Add error boundary to handle client-side exceptions

--- pages/_app.js
+++ pages/_app.js
@@ -6,22 +6,25 @@ import {
   CommentProvider, 
   LikeProvider 
 } from '@/firebase/contexts';
+import ErrorBoundary from '@/components/ErrorBoundary';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <ArtworkProvider>
-          <TourProvider>
-            <CommentProvider>
-              <LikeProvider>
-                <Component {...pageProps} />
-              </LikeProvider>
-            </CommentProvider>
-          </TourProvider>
-        </ArtworkProvider>
-      </UserProvider>
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <UserProvider>
+          <ArtworkProvider>
+            <TourProvider>
+              <CommentProvider>
+                <LikeProvider>
+                  <Component {...pageProps} />
+                </LikeProvider>
+              </CommentProvider>
+            </TourProvider>
+          </ArtworkProvider>
+        </UserProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }

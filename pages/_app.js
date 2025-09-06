695cf4542931a2b0e018be8c74b2b410936c46a1
Goto Yuriko <gotoyuriko95@gmail.com>
2025-09-07 00:52:55 +0800

    Temporarily remove ErrorBoundary to isolate issue

--- pages/_app.js
+++ pages/_app.js
@@ -6,25 +6,22 @@ import {
   CommentProvider, 
   LikeProvider 
 } from '@/firebase/contexts';
-import ErrorBoundary from '@/components/ErrorBoundary';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ErrorBoundary>
-      <AuthProvider>
-        <UserProvider>
-          <ArtworkProvider>
-            <TourProvider>
-              <CommentProvider>
-                <LikeProvider>
-                  <Component {...pageProps} />
-                </LikeProvider>
-              </CommentProvider>
-            </TourProvider>
-          </ArtworkProvider>
-        </UserProvider>
-      </AuthProvider>
-    </ErrorBoundary>
+    <AuthProvider>
+      <UserProvider>
+        <ArtworkProvider>
+          <TourProvider>
+            <CommentProvider>
+              <LikeProvider>
+                <Component {...pageProps} />
+              </LikeProvider>
+            </CommentProvider>
+          </TourProvider>
+        </ArtworkProvider>
+      </UserProvider>
+    </AuthProvider>
   );
 }
